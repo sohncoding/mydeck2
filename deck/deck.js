048b81b3170dc718e165b3e191db29bf7348661b
sohncoding <sohncoding@gmail.com>
2024-12-31 17:56:41 +0900

    test2

--- deck/deck.js
+++ deck/deck.js
@@ -4,9 +4,19 @@ let myData = {
   };
   
   const allCharacters = [
-    { id: "101", name: "슬라임", img: "../images/101.png" },
-    { id: "102", name: "고블린", img: "../images/102.png" },
-    { id: "103", name: "드래곤", img: "../images/103.png" }
+    { id: "101", name: "스노우 화이트", img: "../images/101.png" },
+    { id: "102", name: "이사벨", img: "../images/102.png" },
+    { id: "103", name: "라푼젤", img: "../images/103.png" },
+    { id: "104", name: "홍련", img: "../images/104.png" },
+    { id: "105", name: "하란", img: "../images/105.png" },
+    { id: "106", name: "노아", img: "../images/106.png" },
+    { id: "107", name: "모더니아", img: "../images/107.png" },
+    { id: "108", name: "니힐리스타", img: "../images/108.png" },
+    { id: "109", name: "도로시", img: "../images/109.png" },
+    { id: "110", name: "레드 후드", img: "../images/110.png" },
+    { id: "111", name: "스노우 화이트:이노센트 데이즈", img: "../images/111.png" },
+    { id: "112", name: "홍련:흑영", img: "../images/112.png" },
+    { id: "113", name: "크라운", img: "../images/113.png" }
   ];
   
   window.addEventListener("DOMContentLoaded", () => {
